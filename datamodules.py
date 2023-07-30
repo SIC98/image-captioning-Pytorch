a74712f7eab9383717b35733e1dba96ed4464a0b
SIC98 <shininchul98@gmail.com>
2023-07-30 12:53:27 +0900

    Create data module

---
+++ datamodules.py
@@ -0,0 +1,69 @@
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+class COCODataModule(LightningDataModule):
+    def __init__(self):
+        super().__init__()
+        self.train_batch_size = 16
+        self.valid_batch_size = 16
+        self.num_workers = 4
+        self.shuffle = True
+
+        transform = transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]
+            )
+        ])
+
+        self.train_dataset = datasets.CocoCaptions(
+            root='coco2017/train2017',
+            annFile='coco2017/annotations/captions_train2017.json',
+            transform=transform
+        )
+
+        img, target = self.train_dataset[0]
+
+        print("Image Size: ", img.size())
+        print(target)
+
+        self.val_dataset = datasets.CocoCaptions(
+            root='coco2017/val2017',
+            annFile='coco2017/annotations/captions_val2017.json',
+            transform=transform
+        )
+
+    def setup(self, stage=None):
+        pass
+
+    def train_dataloader(self):
+
+        return DataLoader(
+            dataset=self.train_dataset,
+            batch_size=self.train_batch_size,
+            drop_last=True,
+            num_workers=self.num_workers,
+            shuffle=self.shuffle,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.val_dataloader,
+            batch_size=self.valid_batch_size,
+            drop_last=False,
+            num_workers=self.num_workers,
+        )
+
+
+if __name__ == '__main__':
+    datamodule = COCODataModule()
+    dataloader = datamodule.train_dataloader()
+    print(dataloader)
+    for img, target in dataloader:
+        print(img.shape)
+        print(len(target), len(target[0]), target[0][0])
+        break
