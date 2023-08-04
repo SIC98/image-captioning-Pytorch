0079691383f38fc232c4cc5544cec640018bd748
SIC98 <shininchul98@gmail.com>
2023-08-04 20:07:08 +0900

    Add coco dataset

---
+++ cocodatasets.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset
+from torchvision import datasets
+
+
+class CaptionDataset(Dataset):
+    """
+    A PyTorch Dataset class to be used in a PyTorch DataLoader to create batches.
+    """
+
+    def __init__(self, root, annFile, transform, cpi):
+        """
+        :param data_folder: folder where data files are stored
+        :param data_name: base name of processed datasets
+        :param split: split, one of 'TRAIN', 'VAL', or 'TEST'
+        :param transform: image transform pipeline
+        """
+        self.dataset = datasets.CocoCaptions(
+            root=root,
+            annFile=annFile,
+            transform=transform
+        )
+
+        self.dataset_size = len(self.dataset) * cpi
+        self.cpi = cpi
+
+    def __getitem__(self, i):
+        # Remember, the Nth caption corresponds to the (N // captions_per_image)th image
+        img = self.dataset[i // self.cpi][0]
+
+        caption = self.dataset[i // self.cpi][1][i % self.cpi]
+
+        all_captions = self.dataset[i // self.cpi][1][:self.cpi]
+
+        return img, caption, all_captions
+
+    def __len__(self):
+        return self.dataset_size
